3b74c46b64bb204d53e1f9e6d06c5d022ec89350
suheonjoo <suheonj95@naver.com>
2022-03-02 01:35:46 +0900

    220302

--- spring mvc(2)/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring mvc(2)/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,14 +2,17 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000",message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
+    @NotNull //수정 요구사항 추가
     private Long id;
 
     @NotBlank
@@ -20,7 +23,7 @@ public class Item {
     private Integer price;
 
     @NotNull
-    @Max(9999)
+    //@Max(9999) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
