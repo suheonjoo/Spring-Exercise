88015170c5feca5f9aa071f4f8321b8515133c25
suheonjoo <suheonj95@naver.com>
2022-03-03 23:59:24 +0900

    220303

---
+++ spring mvc(2)/login/src/main/java/hello/login/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    //control o 로 오버라이드 호출
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute("logId",uuid);
+
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        Object logId = request.getAttribute("logId");
+        
+    }
+}
--- spring mvc(2)/login/src/main/java/hello/login/web/login/LoginController.java
+++ spring mvc(2)/login/src/main/java/hello/login/web/login/LoginController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -77,7 +74,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -100,6 +97,33 @@ public class LoginController {
         //sessionManager.createSession(loginMember,response );
 
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        //LoginForm에 @Data를 해줘야 get으로 접근 가능
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginMember==null){
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공
+        //세션이 있으면 있는 세션을 반환, 없으면 신규 세션을 생성해서 반환함
+        HttpSession session = request.getSession();
+        //세션의 로그인 회원정보를 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
+
+        //세션 관리자를 통해 세션을 생성하고, 회원데이터를 보관
+        //sessionManager.createSession(loginMember,response );
+
+        return "redirect:"+redirectURL;
     }
 
     //@PostMapping("/logout")
