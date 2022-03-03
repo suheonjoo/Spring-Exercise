70af615a75489b1d1f10c8fdb64ebc8f8fe72b0f
suheonjoo <suheonj95@naver.com>
2022-03-04 00:42:08 +0900

    220303

--- spring mvc(2)/login/src/main/java/hello/login/WebConfig.java
+++ spring mvc(2)/login/src/main/java/hello/login/WebConfig.java
@@ -2,15 +2,27 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LoginInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    //이부분 command n 에서 implement method로 들어감
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","/*.icon","/error");
+    }
 
     @Bean
     public FilterRegistrationBean logFilter(){
--- spring mvc(2)/login/src/main/java/hello/login/web/interceptor/LoginInterceptor.java
+++ spring mvc(2)/login/src/main/java/hello/login/web/interceptor/LoginInterceptor.java
@@ -1,6 +1,7 @@
 package hello.login.web.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,6 +12,8 @@ import java.util.UUID;
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
+    public static final String LOG_ID = "logId";
+
     //control o 로 오버라이드 호출
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -18,19 +21,32 @@ public class LoginInterceptor implements HandlerInterceptor {
         String requestURI = request.getRequestURI();
         String uuid = UUID.randomUUID().toString();
 
-        request.setAttribute("logId",uuid);
+        //상수로 만드는 단축키를 몰라 control t 로 검색해서 상수로 만듦
+        request.setAttribute(LOG_ID,uuid);
 
+        //@RequestMapping를 사용하면 HandlerMethod가 넘어오고
+        //정적 리소스를 사용하면 ResourceHttpRequestHandler가 넘어온다
+        if(handler instanceof HandlerMethod){
+            HandlerMethod hm = (HandlerMethod) handler;//호출할 컨트롤러의 메서드의 모든 정보가 포함되어 있음
+        }
+
+        log.info("REQUEST [{}][{}][{}]",uuid,requestURI,handler);
+        return true;
 
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.info("postHandle [{}]",modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        Object logId = request.getAttribute("logId");
-        
+        String requestURI = request.getRequestURI();
+        Object logId = (String) request.getAttribute("logId");
+        log.info("REQUEST [{}][{}][{}]",logId,requestURI,handler);
+        if(ex!=null){
+            log.error("afterCompletion error!!",ex); //오류는 {}안해줘도 됨
+        }
     }
 }
