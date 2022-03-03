a899b9dc484d6c259e26d521cf2ec0532acbe3ec
suheonjoo <suheonj95@naver.com>
2022-03-03 21:16:30 +0900

    220303

---
+++ spring mvc(2)/login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,8 @@
+package hello.login.web;
+
+//클래서 안에 상수만들려고 만들클래스라 추상클래스로 만들거나 아니면 인터페이스로 쓰는게 더 좋음
+public class SessionConst {
+
+    public static final String LOGIN_MEMBER ="loginMember";
+
+}
--- spring mvc(2)/login/src/main/java/hello/login/web/login/LoginController.java
+++ spring mvc(2)/login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,8 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -22,13 +26,14 @@ import javax.validation.Valid;
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form){
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -49,4 +54,77 @@ public class LoginController {
 
         return "redirect:/";
     }
+
+    //@PostMapping("/login")
+    public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        //LoginForm에 @Data를 해줘야 get으로 접근 가능
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginMember==null){
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공
+
+        //세션 관리자를 통해 세션을 생성하고, 회원데이터를 보관
+        sessionManager.createSession(loginMember,response );
+
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        //LoginForm에 @Data를 해줘야 get으로 접근 가능
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginMember==null){
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공
+        //세션이 있으면 있는 세션을 반환, 없으면 신규 세션을 생성해서 반환함
+        HttpSession session = request.getSession();
+        //세션의 로그인 회원정보를 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
+
+        //세션 관리자를 통해 세션을 생성하고, 회원데이터를 보관
+        //sessionManager.createSession(loginMember,response );
+
+        return "redirect:/";
+    }
+
+    //@PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response,"memberId");
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if(session!=null){
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response,String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
 }
+
+
+
+
