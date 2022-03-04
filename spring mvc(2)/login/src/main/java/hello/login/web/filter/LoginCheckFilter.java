207517d6c878e8967c6778c2f0947841fcbd4d30
suheonjoo <suheonj95@naver.com>
2022-03-04 23:16:20 +0900

    220304

--- spring mvc(2)/login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
+++ spring mvc(2)/login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -28,7 +28,6 @@ public class LoginCheckFilter implements Filter {
                 log.info("인증 체크 로직 실행 {}",requestURI);
                 HttpSession session = httpRequest.getSession(false);
                 if(session==null || session.getAttribute(SessionConst.LOGIN_MEMBER)==null){
-
                     log.info("미인증 사용자 요청",requestURI);
                     //로그인으로 redirect
                     httpResponse.sendRedirect("/login?redirectURL="+requestURI);
@@ -41,10 +40,6 @@ public class LoginCheckFilter implements Filter {
         }finally {
             log.info("인증 체크 필터 종료{} ",requestURI);
         }
-
-
-
-
     }
 
     /**
