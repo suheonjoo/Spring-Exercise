73b1f5f83bc0581e1ebe0e36ac5b11b2530b3af0
suheonjoo <suheonj95@naver.com>
2022-03-06 13:12:51 +0900

    220306

---
+++ spring mvc(2)/exception/src/main/java/hello/exception/exception/UserException.java
@@ -0,0 +1,25 @@
+package hello.exception.exception;
+
+public class UserException extends RuntimeException{
+
+    //control o로 오버라이드 할거 불러옴
+    public UserException() {
+        super();
+    }
+
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UserException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
