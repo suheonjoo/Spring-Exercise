22bac13347b722a820beaab4881899a4aa229d18
suheonjoo <suheonj95@naver.com>
2022-04-01 01:33:18 +0900

    220401

--- spring mvc(2)/typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
+++ spring mvc(2)/typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -15,15 +15,18 @@ public class FormattingConversionServiceTest {
     @Test
     void formattingConversionService(){
         DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
         //컨버터 등록
         conversionService.addConverter(new StringToIpPortConverter());
         conversionService.addConverter(new IpPortToStringConverter());
+
         //포매터 등록
         conversionService.addFormatter(new MyNumberFormatter());
 
         //컨버터 사용
         IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
         assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1",8080));
+
         //포매서 사용
         assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
         assertThat(conversionService.convert("1000", Long.class)).isEqualTo(1000);
